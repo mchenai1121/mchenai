1491b11d97309641251a77db15bbc254119024d9
mchenai1121 <98354191+mchenai1121@users.noreply.github.com>
2022-12-11 14:53:50 +0800

    Add files via upload

--- import_index_constituents.py
+++ import_index_constituents.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import tabula
+import sqlalchemy
 
 frames = tabula.read_pdf("https://github.com/mchenai1121/mchenai/raw/main/con_9Dec22.pdf", lattice = True, pages = "all")
 
 a = frames[0]
 b = a.iloc[0]
-print(b)
 
 for i in frames:
     i.columns = range(i.shape[1])
@@ -13,4 +13,19 @@ for i in frames:
 
 df = pd.concat(frames, ignore_index = True)
 
+df.columns = list(b)
 
+index_constituents = df[["Trade Date", "Stock\rName", "Closing\rPrice", "Weighting\r(%)"]]
+
+index_constituents.rename(columns={"Trade Date":"date", 
+                               "Stock\rName":"index_name", 
+                               "Closing\rPrice":"ticker", 
+                               "Weighting\r(%)":"weighting"},
+                                inplace = True)
+
+conn_string = "postgresql://postgres:Leonhart0322@localhost:5432/postgres"
+
+engine = sqlalchemy.create_engine(conn_string)
+
+
+index_constituents.to_sql("index_constituents", engine, if_exists = "append", schema = "market_data", index = False)
